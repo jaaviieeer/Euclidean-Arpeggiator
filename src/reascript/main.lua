8821836493aa9e07e79a875ebcb2564951fdf1a0
JavierPasamontes <125408453+JavierPasamontes@users.noreply.github.com>
2026-02-05 18:28:57 +0100

    Fixes

--- src/reascript/main.lua
+++ src/reascript/main.lua
@@ -3,12 +3,12 @@ dofile(script_path .. "./bjorklund.lua")
 dofile(script_path .. "./euclideanArp.lua")
 reaper.Undo_BeginBlock()
 reaper.ShowConsoleMsg("=== EUCLIDEAN ARP (TEMPORAL) ===\n")
-local steps = 30
-local pulses = 19
-local note_len_steps = 3 --lenght of the note in steps
+local steps = 15
+local pulses = 9
+local note_len_steps = 1 --lenght of the note in steps
 local gate = 1 --% of the step that uses a note
-local order = 4 --1 up, 2 down, 3 ping pong, 4 random
-local step_len = 3 --0 adaptative, 1 whole, 2 half, 3 quarter, 4 eighth, 5 sixteenth 
+local order = 1 --1 up, 2 down, 3 ping pong, 4 random
+local step_len = 0 --0 adaptative, 1 whole, 2 half, 3 quarter, 4 eighth, 5 sixteenth 
 -- we take the selected item
 local item = reaper.GetSelectedMediaItem(0, 0)
 if not item then
