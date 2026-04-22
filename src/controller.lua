396daed53a2d7fd03fe23816c0530c49a33ce6af
JavierPasamontes <jpasamartin@gmail.com>
2026-04-22 14:01:34 +0200

    Controller as bridge between systems

--- src/controller.lua
+++ src/controller.lua
@@ -7,51 +7,62 @@ local pitch = dofile(script_path .. "./core/pitch.lua")
 local time = dofile(script_path .. "./core/time.lua")
 local generator = dofile(script_path .. "./core/generator.lua")
 local reaperInteraction = dofile(script_path .. "./adapter/offlineAdapter.lua")
+local jsfxInteraction = dofile(script_path .. "./adapter/liveAdapter.lua")
+
 
 function M.apply(config)
-    -- we take the selected item
-    local item = reaper.GetSelectedMediaItem(0, 0)
-    if not item then
-        reaper.ShowConsoleMsg("No selected item")
-        return
-    end
-    -- check if item contains midi
-    local take = reaper.GetActiveTake(item)
-    if not take or not reaper.TakeIsMIDI(take) then
-        reaper.ShowConsoleMsg("The selected item does not contain MIDI")
-        return
-    end
-
-    reaper.Undo_BeginBlock()
-    local timing = reaperInteraction.get_item_timing(take)
-
-    local events, totalPPQNeeded
-
-    if not config.multiple_chords_enabled then
-        local pitches = reaperInteraction.read_pitches_from_take(take)
-        events, totalPPQNeeded = generator.build_events(pitches, config, timing.ppqPerQN,
-            { bjor = bjor, pitch = pitch, time = time })
-    else
-        local chords      = {}
-        local currentPPQ  = timing.itemStartPPQ
-        local intervalPPQ = time.step_length_ppq(timing.ppqPerQN, config.multiple_chord_interval)
-        while currentPPQ < timing.itemEndPPQ do
-            local pitchesInWindow = reaperInteraction.read_pitches_from_window(take, currentPPQ, currentPPQ + intervalPPQ)
-            if #pitchesInWindow > 0 then
-                table.insert(chords, pitchesInWindow)
-            end
-            currentPPQ = currentPPQ + intervalPPQ
+    if config.mode == "live" then
+        local ok, msg = jsfxInteraction.apply(config, { bjor = bjor, pitch = pitch, time = time })
+        if not ok then
+            reaper.ShowConsoleMsg("Error applying live mode: " .. msg)
         end
-        events, totalPPQNeeded = generator.build_events_from_chord_sequence(chords, config, timing.ppqPerQN,
-            { bjor = bjor, pitch = pitch, time = time })
+        return ok, msg
+    else
+        -- we take the selected item
+        local item = reaper.GetSelectedMediaItem(0, 0)
+        if not item then
+            reaper.ShowConsoleMsg("No selected item")
+            return
+        end
+        -- check if item contains midi
+        local take = reaper.GetActiveTake(item)
+        if not take or not reaper.TakeIsMIDI(take) then
+            reaper.ShowConsoleMsg("The selected item does not contain MIDI")
+            return
+        end
+
+        reaper.Undo_BeginBlock()
+        local timing = reaperInteraction.get_item_timing(take)
+
+        local events, totalPPQNeeded
+
+        if not config.multiple_chords_enabled then
+            local pitches = reaperInteraction.read_pitches_from_take(take)
+            events, totalPPQNeeded = generator.build_events(pitches, config, timing.ppqPerQN,
+                { bjor = bjor, pitch = pitch, time = time })
+        else
+            local chords      = {}
+            local currentPPQ  = timing.itemStartPPQ
+            local intervalPPQ = time.step_length_ppq(timing.ppqPerQN, config.multiple_chord_interval)
+            while currentPPQ < timing.itemEndPPQ do
+                local pitchesInWindow = reaperInteraction.read_pitches_from_window(take, currentPPQ,
+                    currentPPQ + intervalPPQ)
+                if #pitchesInWindow > 0 then
+                    table.insert(chords, pitchesInWindow)
+                end
+                currentPPQ = currentPPQ + intervalPPQ
+            end
+            events, totalPPQNeeded = generator.build_events_from_chord_sequence(chords, config, timing.ppqPerQN,
+                { bjor = bjor, pitch = pitch, time = time })
+        end
+
+        reaperInteraction.clear_notes_from_take(take)
+        reaperInteraction.set_item_length_for_ppq(timing, take, totalPPQNeeded)
+        reaperInteraction.insert_events(take, timing, events)
+
+        reaper.Undo_EndBlock("Euclidean Arp", -1)
+        return true, "OK"
     end
-
-    reaperInteraction.clear_notes_from_take(take)
-    reaperInteraction.set_item_length_for_ppq(timing, take, totalPPQNeeded)
-    reaperInteraction.insert_events(take, timing, events)
-
-    reaper.Undo_EndBlock("Euclidean Arp", -1)
-    return true, "OK"
 end
 
 return M
