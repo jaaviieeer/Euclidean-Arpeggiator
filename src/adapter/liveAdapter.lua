7424c1d46ec959edd3018a7fc58d3cd4cb45653e
JavierPasamontes <jpasamartin@gmail.com>
2026-04-23 13:18:24 +0200

    Finding or inserting jsfx in adapter

--- src/adapter/liveAdapter.lua
+++ src/adapter/liveAdapter.lua
@@ -15,8 +15,20 @@ function M.apply(track, config, dependencies)
     local steps = tonumber(config.steps) or 0
     local pulses = tonumber(config.pulses) or 0
     local note_fraction = tonumber(config.note_fraction) or (1 / 4)
-    
-
+    local fx_count = reaper.TrackFX_GetCount(track)
+    local fx_index = -1
+    for i = 0, fx_count - 1 do
+        local retval, fx = reaper.TrackFX_GetFXName(track, i)
+        if retval and fx:find(JSFX_NAME, 1, true) then
+            fx_index = i
+        end
+    end
+    if fx_index == -1 then
+        fx_index = reaper.TrackFX_AddByName(track, JSFX_NAME, false, -1)
+        if fx_index < 0 then
+            return nil, "Could not insert JSFX: " .. JSFX_NAME
+        end
+    end
 end
 
 return M
